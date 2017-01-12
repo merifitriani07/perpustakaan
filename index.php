cf7825ce5b1e7554d2c58a0e4c67469408d77f04
Meri fitriani <merifitriani02@gmail.com>
2017-01-12 08:59:00 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<form action="login.php" class="form-horizontal" method="post">
+<label class="username">Username</label>
+    <input name="username" placeholder="Username Anda" type="text" />
+    <label class="password">Password</label>        
+    <input name="pass" placeholder="Password" type="password" />
+    <button class="btn" name="submit" type="submit">Login</button>
+</form>
